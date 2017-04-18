63aeb95e5b4720b137557ee7ed01598e26c3adad
Chris Rumble <crumble@mavenlink.com>
2017-04-18 12:01:05 -0700

    Rough first draft

--- app/assets/javascripts/prototypes/emptystates_prototype.js
+++ app/assets/javascripts/prototypes/emptystates_prototype.js
@@ -4,4 +4,17 @@ $( document ).ready(function() {
     $(".load.one").addClass("hide");
     $(".load.two").addClass("loaded");
   });
+  document.getElementById("my-btn-too").addEventListener("click", function(){
+    $(".load.two").addClass("hide");
+    $(".load.three").addClass("loaded");
+  });
+  document.getElementById("onboard-one").addEventListener("click", function(){
+    $("#onboard-one").addClass("left");
+    $("#onboard-two").addClass("left");
+  });
+  document.getElementById("onboard-two").addEventListener("click", function(){
+    $("#onboard-two").addClass("left-more");
+    $("#onboard-three").addClass("left");
+    $(".onboard-top").addClass("taller");
+  });
 });
