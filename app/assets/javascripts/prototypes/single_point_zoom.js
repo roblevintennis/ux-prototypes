ebb3e1f9c9e4d3a870b821ced39ec1b1ae5a4366
Rob Levin <roblevintennis@gmail.com>
2017-05-10 12:31:26 -0700

    First zoom in

--- app/assets/javascripts/prototypes/single_point_zoom.js
+++ app/assets/javascripts/prototypes/single_point_zoom.js
@@ -6,27 +6,46 @@ $( document ).ready(function() {
   //   modal: showModal
   // });
 
-  function isFirstProfileIcon(ev) {
-    const x = ev.clientX;
-    const y = ev.clientY;
-    return (x > 490 && x < 540 && y > 220 && y < 255);
+  function isFirstProfileIcon(self, ev) {
+    const offset = $(self).offset();
+    const relativeX = (ev.pageX - offset.left);
+    const relativeY = (ev.pageY - offset.top);
+
+    console.log("X: " + relativeX + "  Y: " + relativeY);
+    return (relativeX > 300 && relativeX < 360 && relativeY > 310 && relativeY < 350);
   }
 
-  function isSecondProfileIcon(ev) {
-    const x = ev.clientX;
-    const y = ev.clientY;
-    return (x > 490 && x < 540 && y > 260 && y < 300);
+  function isSecondProfileIcon(self, ev) {
+    const offset = $(self).offset();
+    const relativeX = (ev.pageX - offset.left);
+    const relativeY = (ev.pageY - offset.top);
+
+    console.log("X: " + relativeX + "  Y: " + relativeY);
+    return (relativeX > 300 && relativeX < 360 && relativeY > 360 && relativeY < 400);
   }
+
+  function reset() {
+      $('.master-planning').removeClass('hidden');
+      $('.resource-picker').addClass('hidden').removeClass('zoom-in');
+  }
+
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) { // escape key maps to keycode `27`
+      reset();
+    }
+  });
 
   $(".master-planning").on("click", function(ev) {
-    console.log("x: " + ev.clientX);
-    console.log("y: " + ev.clientY);
+    // console.log("x: " + ev.clientX);
+    // console.log("y: " + ev.clientY);
 
-    if(isFirstProfileIcon(ev)) {
+    if(isFirstProfileIcon(this, ev)) {
       console.log("FIRST icon .. DO ZOOM IN")
+      $('.master-planning').addClass('hidden');
+      $('.resource-picker').removeClass('hidden').addClass('zoom-in');
     }
 
-    if(isSecondProfileIcon(ev)) {
+    if(isSecondProfileIcon(this, ev)) {
       console.log("Second icon .. DO SLIDE IN FROM Corner")
     }
 
